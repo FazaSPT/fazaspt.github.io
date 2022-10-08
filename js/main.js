571a2499e896a8c1189e9eb6f9d0782f356682e0
FazaSPT <evriliantiniandrian7@gmail.com>
2022-10-08 16:05:36 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const txtElement = ['Student | Software Engineer'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function type(){
+
+    if(count == txtElement.length){
+        count = 0;
+    }
+
+    currentTxt = txtElement[count];
+
+    words = currentTxt.slice(0, ++txtIndex);
+    document.querySelector('.auto-type').textContent = words;
+
+    if(words.length == currentTxt.length){
+        count++;
+        txtIndex = 0;
+    }
+
+    setTimeout(type, 300);
+})();
